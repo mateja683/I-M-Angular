796130ee623fd0fe1b75e66e3e774e676755362f
hasulica <raduasandei@gmail.com>
2015-12-01 16:14:36 +0000

    data binding

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -8,7 +8,29 @@ describe('GitUserSearchController', function(){
   }));
 
   it('initializes with an empty search result and term', function(){
-    expect(ctrl.searchResult).toBeUndefined();
+    expect(ctrl.searchResults).toBeUndefined();
     expect(ctrl.searchTerm).toBeUndefined();
+  });
+
+  describe('when searching for a user', function() {
+
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      },
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
+
+    it('displays search results', function() {
+      ctrl.searchTerm = "Hello";
+      ctrl.doSearch();
+      expect(ctrl.searchResults.items).toEqual(items);
+    });
   });
 });
