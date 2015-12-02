34d4a93eb469ebcffca11d16c90fdc1f52691e6f
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2015-12-02 15:47:16 +0000

    multiple protractor tests

--- test/e2e/githubProfileFeatures.js
+++ test/e2e/githubProfileFeatures.js
@@ -1,12 +1,21 @@
 describe('Github Profile Finder', function() {
-  it('finds profiles', function() {
+
+  var searchBox = element(by.model('searchCtrl.searchTerm'));
+  var searchButton = element(by.className('btn'));
+
+  beforeEach(function() {
     browser.get('http://localhost:8080');
+  });
 
+  it ('has a title', function() {
+    expect(browser.getTitle()).toEqual('Github user search');
+  });
 
-    element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
-    element(by.className('btn')).click();
+  it('finds profiles', function() {
+    searchBox.sendKeys('mateja6');
+    searchButton.click();
 
-    expect(element(by.binding('user.login')).getText()).
-      toEqual('spike01');
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.last().getText()).toEqual('mateja683');
     });
   });
